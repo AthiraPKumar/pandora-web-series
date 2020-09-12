449d78ca199eef39d04e2ed9f602e648793f29b5
AthiraPKumar <athirapkumar16@gmail.com>
2020-09-13 01:10:55 +0530

    Updates

---
+++ precache-manifest.70c6761d584316fa14b4b223935f5b74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38244916c524876d7ffe16b722cf9cd0",
+    "url": "/pandora-web-series/index.html"
+  },
+  {
+    "revision": "cd7e8cb7cdb80503449b",
+    "url": "/pandora-web-series/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d716539d0acec3398e48",
+    "url": "/pandora-web-series/static/css/main.a021de11.chunk.css"
+  },
+  {
+    "revision": "cd7e8cb7cdb80503449b",
+    "url": "/pandora-web-series/static/js/2.46ec0941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pandora-web-series/static/js/2.46ec0941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d716539d0acec3398e48",
+    "url": "/pandora-web-series/static/js/main.82bae2e4.chunk.js"
+  },
+  {
+    "revision": "0a6781963648a217e7d5",
+    "url": "/pandora-web-series/static/js/runtime-main.09570202.js"
+  }
+]);
